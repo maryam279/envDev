cc43c496182d62bcf1e84bcde29f720d3e84145a
maryam279 <l1f21bsse0516@ucp.edu.pk>
2025-01-21 17:26:59 +0500

    Create file.js

---
+++ file.js
@@ -0,0 +1,6 @@
+// This JavaScript file is for any future interactive elements
+// For now, it will simply log a message when the page loads
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Fashion Blog Loaded!");
+});
